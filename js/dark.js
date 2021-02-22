ec6e49e712b4b7aef49463c5b0bc93f57a9d5efe
Adriel Gama <adrielgama@gmail.com>
2021-02-22 11:25:23 -0300

    new switch darkmode toogle

--- js/dark.js
+++ js/dark.js
@@ -1,6 +1,4 @@
-const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]'
-);
+const toggleSwitch = document.querySelector('.dayNight input[type="checkbox"]');
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
